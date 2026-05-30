8359819a1e6980a2e1b00ffb21a2d351313c0463
emirhansumer <emirhansmr@gmail.com>
2026-05-31 02:50:41 +0300

    chore: yönetici hesabı görünen adı 'Site Yöneticisi' (rozet tekrarını önle)

--- Data/DbSeeder.cs
+++ Data/DbSeeder.cs
@@ -20,7 +20,7 @@ public static class DbSeeder
         {
             db.Kullanicilar.Add(new Kullanici
             {
-                AdSoyad = "Sistem Yöneticisi",
+                AdSoyad = "Site Yöneticisi",
                 Email = AdminEmail,
                 SifreHash = BCrypt.Net.BCrypt.HashPassword(AdminSifre),
                 IsAdmin = true,
